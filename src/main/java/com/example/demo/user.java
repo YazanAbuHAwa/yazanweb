6039e692b96fab1973b37cc926137018aa98d245
YazanAbuHAwa <47882852+YazanAbuHAwa@users.noreply.github.com>
2021-06-15 02:19:19 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/user.java
@@ -0,0 +1,66 @@
+package com.example.demo;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@Entity(name="user")
+@Table(name="users")
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class user implements Serializable{
+
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id  ;
+	@Column
+	private String username;
+	@Column
+	private String email;
+	@Column
+	private String password;
+	public user() {}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+	
+	
+	
+}
---
+++ src/main/java/com/example/demo/userService.java
@@ -0,0 +1,39 @@
+package com.example.demo;
+
+import java.util.Collection;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.userDao;
+import com.example.demo.user;
+@Service
+@Transactional
+public class userService {
+	@Autowired
+	private userDao dao;
+	
+	public user saveUser(user u)
+	{
+		
+		return dao.save(u);
+	}
+	public Collection<user> getAllUsers()
+	{
+		return dao.findAll();
+	}
+	public void removeUser(int id )
+	{
+		dao.deleteById(id);
+	}
+	public user updateUser(user u )
+	{
+		return dao.save(u);
+	}
+	public user login(String email, String password)
+	{
+		return dao.login(email, password);
+	}
+}
